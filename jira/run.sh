ff931ee7b8554c5de24d1bac69820f27e87dcba6
Ivan Tichy <ivan.tichy@gmail.com>
2016-05-03 16:58:29 +0200

    some workd done

--- jira/run.sh
+++ jira/run.sh
@@ -1,5 +1,5 @@
-docker run -it -p 80:8080  \
+docker run -d -p 8080:8080  \
 -v /var/docker-data/postgres:/var/lib/postgresql/9.4/main \
 -v  /var/docker-data/jira-app:/var/atlassian/jira-app \
 -v  /var/docker-data/jira-home:/var/atlassian/jira-home \
-ivantichy/jira-clean
+ivantichy/jira-clean "$@"
